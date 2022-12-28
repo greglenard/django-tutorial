0ac012ab77ef0c30546eb23711f245eda67ffdf0
Greg <gregory.lenard@colorado.edu>
2022-12-28 13:18:09 -0700

    add results and details html, views, urls

--- tut/settings.py
+++ tut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-33(523$^blmavhjswu*-+)_f#$x1ajffk!r-f80b06@%z!bbiw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
